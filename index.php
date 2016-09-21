4317c6e527f4acce629f4a370efb16f953ff2592
Kirin Anton <vertikallimit@mail.ru>
2016-09-22 03:01:46 +0400

    commit 4

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-﻿<?php
-header ("Content-Type: text/html; charset=utf-8");
-echo "<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
 <title>Open Server</title>
 </head>
-<body style=\"background: url(fon.png) top left repeat-x\">
+<body style="background: url(fon.png) top left repeat-x">
 <center>
-<br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Страница для проверки работы Git'a</span>
+<br><br><br><span style="font-size: 32px; color: #333; font-family: Verdana, Arial;">Страница для проверки работы Git'a</span>
 <p>234</p>
 </center>
 </body>
-</html>";
-?>
+</html>
