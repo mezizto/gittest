84e2c77534904a524d1d805a097603d112cbedc1
Kirin Anton <vertikallimit@mail.ru>
2016-09-22 02:34:05 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<?php
+header ("Content-Type: text/html; charset=utf-8");
+echo "<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+<title>Open Server</title>
+</head>
+<body style=\"background: url(fon.png) top left repeat-x\">
+<center>
+<br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Страница для проверки работы Git'a</span>
+<h1>Редактирование файла произошлооо!</h1>
+<h1>Редактирование файла произошлооо! 2</h1>
+</center>
+</body>
+</html>";
+?>
