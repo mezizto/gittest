3c5ff7412c87ac20aed58725cbfc8034ce4e2465
Kirin Anton <vertikallimit@mail.ru>
2016-09-22 02:40:23 +0400

    third commit

--- index.php
+++ index.php
@@ -9,10 +9,7 @@ echo "<!DOCTYPE html>
 <body style=\"background: url(fon.png) top left repeat-x\">
 <center>
 <br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Страница для проверки работы Git'a</span>
-<h1>Редактирование файла произошлооо!</h1>
-<h1>Редактирование файла произошлооо! 2</h1>
-
-<h1>Редактирование файла произошлооо! 3</h1>
+<p>234</p>
 </center>
 </body>
 </html>";
